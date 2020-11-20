926a4ec4586c7e0139e630b936d14d91d2466013
Tony Perez <tonper19@gmail.com>
2020-11-20 18:08:19 +0200

    0004. Templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -5,11 +5,23 @@ from django.http import HttpResponse
 
 
 def home_view(request, *args, **kwargs):
+    print(f'Calling {__name__}.home_view() ...')
     print(f'request.user: {request.user}')
     print(f'args: {args}')
     print(f'kwargs: {kwargs}')
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {})
 
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Contact View</h1>")
+    print(f'Calling {__name__}.contact_view() ...')
+    print(f'request.user: {request.user}')
+    # return HttpResponse("<h1>Contact View</h1>")
+    return render(request, "contact.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    print(f'Calling {__name__}.about_view() ...')
+    print(f'request.user: {request.user}')
+    # return HttpResponse("<h1>Contact View</h1>")
+    return render(request, "about.html", {})
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view, contact_view
+from pages.views import home_view, contact_view, about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('contact/', contact_view, name='contact'),
+    path('about/', about_view, name='about'),
 ]
