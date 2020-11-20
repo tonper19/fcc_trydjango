24e300b1df4acab94f40bd5d8bf90e1c8346023c
Tony Perez <tonper19@gmail.com>
2020-11-20 13:32:52 +0200

    0002. Models

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party
+
+    # own
+    'products',
+
 ]
 
 MIDDLEWARE = [
