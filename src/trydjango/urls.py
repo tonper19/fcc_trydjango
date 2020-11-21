3501ec94b3475e49791ef3270574de65a1c70b43
Tony Perez <tonper19@gmail.com>
2020-11-21 19:49:34 +0200

    Render data from the DB with a model

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, contact_view, about_view
+from products.views import product_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('contact/', contact_view, name='contact'),
+    path('product/', product_detail_view, name='product'),
     path('about/', about_view, name='about'),
 ]
