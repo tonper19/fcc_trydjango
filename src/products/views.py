f02c83fece6c5b7ba7ad972934d579e2247d38c6
Tony Perez <tonper19@gmail.com>
2020-11-22 18:03:28 +0200

    0019. Handle DoesNotExists

--- src/products/views.py
+++ src/products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Product
 from .forms import ProductForm, RawProductForm
 # Create your views here.
@@ -36,7 +37,13 @@ def product_create_view(request):
 
 
 def dynamic_lookup_view(request, id):
-    obj = Product.objects.get(id=id)
+    # obj = Product.objects.get(id=id)
+    obj = get_object_or_404(Product, id=id)
+    # try:
+    #     obj = Product.objects.get(id=id)
+    # except Product.DoesNotExist:
+    #     raise Http404
+
     context = {
         "object": obj
     }
