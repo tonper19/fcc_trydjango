1c5cdca5ecb7885a9c183dbc4c6a0cab9614ec7e
Tony Perez <tonper19@gmail.com>
2020-11-22 13:40:28 +0200

    0013. Raw HTML form

--- src/products/views.py
+++ src/products/views.py
@@ -18,13 +18,22 @@ def product_detail_view(request):
     return render(request, "products/product_detail.html", context)
 
 
-def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
 
-    context = {
-        'form': form
-    }
+#     context = {
+#         'form': form
+#     }
+#     return render(request, "products/product_create.html", context)
+
+def product_create_view(request):
+    # print(request.POST)
+    if request.method == "POST":
+        my_new_title = request.POST.get("title")
+        print(my_new_title)
+
+    context = {}
     return render(request, "products/product_create.html", context)
