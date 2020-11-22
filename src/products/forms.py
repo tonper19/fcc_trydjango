e3902b3c49eaca1f78a1cb6c38926d9c2e4b0da1
Tony Perez <tonper19@gmail.com>
2020-11-22 16:07:25 +0200

    0016. Form validation methods

--- src/products/forms.py
+++ src/products/forms.py
@@ -3,6 +3,28 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(
+        label='',
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Enter the title"
+            }
+        )
+    )
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Enter the description",
+                "class": "html-new-class-name html-another-class",
+                "id": "html-id",
+                "rows": 3,
+                "cols": 10
+            }
+        )
+    )
+    price = forms.DecimalField(initial=199.99)
+
     class Meta:
         model = Product
         fields = [
@@ -10,6 +32,18 @@ class ProductForm(forms.ModelForm):
             'description',
             'price',
         ]
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if not "TA" in title:
+            raise forms.ValidationError("Title must contain the word TA")
+        return title
+
+    def clean_price(self, *args, **kwargs):
+        price = self.cleaned_data.get("price")
+        if not 1 <= price <= 10000:
+            raise forms.ValidationError("price must be between 1 and 10000")
+        return price
 
 
 class RawProductForm(forms.Form):
--- src/products/views.py
+++ src/products/views.py
@@ -18,16 +18,16 @@ def product_detail_view(request):
     return render(request, "products/product_detail.html", context)
 
 
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm()
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
 
-#     context = {
-#         'form': form
-#     }
-#     return render(request, "products/product_create.html", context)
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
 
 # def product_create_view(request):
 #     # print(request.POST)
@@ -38,19 +38,19 @@ def product_detail_view(request):
 #     context = {}
 #     return render(request, "products/product_create.html", context)
 
-def product_create_view(request):
-    my_form = RawProductForm()
-    if request.method == "POST":
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            # now the data is good
-            print(my_form.cleaned_data)
-            # pass dictionary as a bunch of arguments
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
+# def product_create_view(request):
+#     my_form = RawProductForm()
+#     if request.method == "POST":
+#         my_form = RawProductForm(request.POST)
+#         if my_form.is_valid():
+#             # now the data is good
+#             print(my_form.cleaned_data)
+#             # pass dictionary as a bunch of arguments
+#             Product.objects.create(**my_form.cleaned_data)
+#         else:
+#             print(my_form.errors)
 
-    context = {
-        "form": my_form
-    }
-    return render(request, "products/product_create.html", context)
+#     context = {
+#         "form": my_form
+#     }
+#     return render(request, "products/product_create.html", context)
