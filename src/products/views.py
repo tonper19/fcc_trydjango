d0b57b923a7ca0d2e9e34ed93043233165f8fd7b
Tony Perez <tonper19@gmail.com>
2020-11-22 19:05:11 +0200

    0021. View of a list of DB objects

--- src/products/views.py
+++ src/products/views.py
@@ -60,6 +60,14 @@ def product_delete_view(request, id):
     }
     return render(request, "products/product_delete.html", context)
 
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "products/product_list.html", context)
+
 # def product_create_view(request):
 #     # print(request.POST)
 #     if request.method == "POST":
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -20,6 +20,7 @@ from products.views import (
     product_detail_view,
     product_delete_view,
     product_create_view,
+    product_list_view,
     dynamic_lookup_view)
 
 urlpatterns = [
@@ -30,5 +31,6 @@ urlpatterns = [
     path('product/', product_detail_view),
     path('products/<int:id>/', dynamic_lookup_view, name='product'),
     path('products/<int:id>/delete/', product_delete_view, name='product-delete'),
+    path('products/', product_list_view, name='product-list'),
     path('about/', about_view, name='about'),
 ]
