ef982ca9b18d166cd77acda01e0ea77e5661d5e0
Tony Perez <tonper19@gmail.com>
2020-11-23 18:24:56 +0200

    0022. Dynamic linking of URLs

--- src/products/models.py
+++ src/products/models.py
@@ -9,3 +9,6 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=10000)
     summary = models.TextField(blank=True, null=False)
     featured = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return f"/products/{self.id}/"
