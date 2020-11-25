02d1fe6ec9736cd6ecba562c14e498320de76501
Tony Perez <tonper19@gmail.com>
2020-11-25 20:27:56 +0200

    0027. Class Based Views - DetailView

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from .views import (
-    ArticleListView
+    ArticleListView,
+    ArticleDetailView
 )
 
 app_name = 'articles'
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='article-list')
+    path('', ArticleListView.as_view(), name='article-list'),
+    # this is the standard: use pk as the argument on a DetailView
+    #    path('<int:pk>', ArticleDetailView.as_view(), name='article-detail')
+    # to override the standard pk arg, in the views.py,
+    #    path('<int:id>', ArticleDetailView.as_view(), name='article-detail'),
+    path('<str:title>', ArticleDetailView.as_view(), name='article-detail'),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # 2020-11-24
 from django.views.generic import (
     CreateView,
@@ -14,3 +14,20 @@ class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
     # looks for <blog>/<modulename>_list.html otherwise put template_name as above
     queryset = Article.objects.all()
+
+
+class ArticleDetailView(DetailView):
+    template_name = 'articles/article_detail.html'
+    # looks for <blog>/<modulename>_list.html otherwise put template_name as above
+
+    # this may be a subset, in this case we are retreiving all data
+    # queryset = Article.objects.all()
+
+    # to override pk argument on the urls.py by the id:
+    # def get_object(self):
+    #     id_ = self.kwargs.get("id")
+    #     return get_object_or_404(Article, id=id_)
+    def get_object(self):
+        title_ = self.kwargs.get("title")
+        return get_object_or_404(Article, title=title_)
+# The Cretan Chronicles
